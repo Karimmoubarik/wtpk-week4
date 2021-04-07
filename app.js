4926ae20091b08703c6e5defe28cc835f9ec2da1
Karim <karim.moubarik@metropolia.fi>
2021-04-07 15:35:06 +0300

    'sets'

--- app.js
+++ app.js
@@ -2,15 +2,24 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const passport = require('./utils/pass')
 const app = express();
 const port = 3000;
+
+const loggedIn = (req, res, next) => {
+  if(req.user){
+    next()
+  } else {
+    res.redirect('/form')
+  }
+};
 
 // älä tee näin projektissa
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-const username = 'foo';
+const username = 'foo:';
 const password = 'bar';
 
 app.use(cookieParser());
@@ -21,20 +30,19 @@ app.use(session({
   cookie: {maxAge: 60 * 60 * 24},
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 // älä tee näin projektissa
 
-app.post('/login', (req, res) => {
-  const uname = req.body.username;
-  const passwd = req.body.password;
-  if (uname === username && passwd === password) {
-    req.session.kirjautunut = true;
-    res.redirect('/secret');
-  } else {
-    res.redirect('/form');
-  }
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/form'}),
+    (req, res) => {
+      console.log('success');
+      res.redirect('/secret');
 });
 
 // älä tee näin projektissa
@@ -49,12 +57,8 @@ app.get('/form', (req, res) => {
   res.render('form');
 });
 
-app.get('/secret', (req, res) => {
-  if (req.session.kirjautunut) {
-    res.render('secret');
-  } else {
-    res.redirect('/form');
-  }
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret')
 });
 
 // älä tee näin projektissa
@@ -73,6 +77,11 @@ app.get('/readCookie', (req, res) => {
 app.get('/deleteCookie', (req, res) => {
   res.clearCookie('color');
   res.send('cookie deleted');
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 });
 
 // älä tee näin projektissa
